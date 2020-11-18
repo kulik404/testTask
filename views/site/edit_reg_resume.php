63f936c44c5271334703fee35e8034f354da6b97
kulik404 <kulikalexey@mail.ru>
2020-11-18 19:43:44 +0300

    18_11_19

--- views/site/edit_reg_resume.php
+++ views/site/edit_reg_resume.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 
 use yii\bootstrap4\ActiveForm;
-
+use  yii\helpers\Html;
 
 $this->title = 'Создание нового резюме';
 $this->registerCssFile('css/jquery.nselect.css'); 
@@ -13,8 +13,6 @@ $this->registerJsFile('js/jquery.nselect.min.js');
 $this->registerJsFile('js/bootstrap-datepicker.js');
 $this->registerJsFile('js/bootstrap-datepicker.ru.min.js'); 
 $this->registerJsFile('js/jquery-editable-select.js'); 
-
- 
 
 ?>
 
@@ -75,21 +73,23 @@ $this->registerJsFile('js/jquery-editable-select.js');
                             <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true, 'class' => 'dor-input w100'])->label(false) ; ?>
                         </div>
                     </div>
+                   
                     <div class="row mb24">
                         <div class="col-lg-2 col-md-3 dflex-acenter">
                             <div class="paragraph">Дата рождения</div>
                         </div>
                         <div class="col-lg-3 col-md-4 col-11">
                             <div class="datepicker-wrap input-group date">
-                                <!-- <input type="text" class="dor-input dpicker datepicker-input"> -->
-                                <?= $form->field($model, 'bdate', ['options' => [
-                'tag' => false,
+                                <input type="text" class="dor-input dpicker datepicker-input">
+                                <?//= $form->field($model, 'bdate', ['options' => [
+                //'tag' => false,
                // 'class'=> 'datepicker-wrap input-group date'
-            ]])->Input('text',['class' => 'form-control dor-input dpicker datepicker-input'])->label(false) ; ?>                                
+           // ]])->Input('text',['class' => 'form-control dor-input dpicker datepicker-input'])->label(false) ; ?>                                
                                 <img src="images/mdi_calendar_today.svg" alt="">
                             </div>
                         </div>
                     </div>
+
                     <div class="row mb16">
                         <div class="col-lg-2 col-md-3 dflex-acenter">
                             <div class="paragraph">Пол</div>
@@ -223,60 +223,59 @@ $this->registerJsFile('js/jquery-editable-select.js');
                         </div>
                         <div class="col-lg-3 col-md-4 col-11">
                             <div class="profile-info">
-
-                                <!--  -->
-                                <? $it = [] ?>
-                                <?= $form->field($model, 'fEmp', [
-                                    //'options' => ['class'=> 'form-check d-flex'],
-                                    'checkTemplate' => 
-                                    '<div class="form-check d-flex">
-                                        {input}
-                                        {label}
-                                        {error}
-                                        {hint}
-                                    </div>',])
-                                        ->checkboxList([ 'class' => 'form-control form-check-input',
-                                            //'id' => 'exampleCheck1',
-                                            //'tag' => false,
-                                        ])
-                                         ; ?>
-                                <!--  -->
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck1" name="resume[fEmp]"> -->
+                                    <?= $form->field($model, 'fEmp',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck1'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck1"></label>
                                     <label for="exampleCheck1" class="profile-info__check-text job-resolution-checkbox">Полная
                                         занятость</label>
                                 </div>
-
-
-                                <!-- <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck2">
-
+                                <div class="form-check d-flex">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck2"> -->
+                                    <?= $form->field($model, 'pEmp',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck2'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck2"></label>
                                     <label for="exampleCheck2" class="profile-info__check-text job-resolution-checkbox">Частичная
                                         занятость</label>
                                 </div>
-
-
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck3">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck3"> -->
+                                    <?= $form->field($model, 'tEmp',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck3'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck3"></label>
                                     <label for="exampleCheck3" class="profile-info__check-text job-resolution-checkbox">Проектная/Временная
                                         работа</label>
                                 </div>
-
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck4">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck4"> -->
+                                    <?= $form->field($model, 'vEmp',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck4'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck4"></label>
                                     <label for="exampleCheck4" class="profile-info__check-text job-resolution-checkbox">Волонтёрство</label>
                                 </div>
-
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck5">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck5"> -->
+                                    <?= $form->field($model, 'iEmp',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck5'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck5"></label>
                                     <label for="exampleCheck5" class="profile-info__check-text job-resolution-checkbox">Стажировка</label>
-                                </div> -->
+                                </div>
 
 
                             </div>
@@ -289,31 +288,56 @@ $this->registerJsFile('js/jquery-editable-select.js');
                         <div class="col-lg-3 col-md-4 col-11">
                             <div class="profile-info">
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck6">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck6"> -->
+                                    <?= $form->field($model, 'fSchedule',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck6'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck6"></label>
                                     <label for="exampleCheck6" class="profile-info__check-text job-resolution-checkbox">Полный
                                         день</label>
                                 </div>
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck7">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck7"> -->
+                                    <?= $form->field($model, 'sSchedule',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck7'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck7"></label>
                                     <label for="exampleCheck7" class="profile-info__check-text job-resolution-checkbox">Сменный
                                         график</label>
                                 </div>
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck8">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck8"> -->
+                                    <?= $form->field($model, 'flexSchedule',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck8'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck8"></label>
                                     <label for="exampleCheck8" class="profile-info__check-text job-resolution-checkbox">Гибкий
                                         график</label>
                                 </div>
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck9">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck9"> -->
+                                    <?= $form->field($model, 'remSchedule',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck9'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck9"></label>
                                     <label for="exampleCheck9" class="profile-info__check-text job-resolution-checkbox">Удалённая
                                         работа</label>
                                 </div>
                                 <div class="form-check d-flex">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck10">
+                                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck10"> -->
+                                    <?= $form->field($model, 'rSchedule',['checkTemplate' => '{input}',
+                                                                    'options' => ['tag' => false]                            ])
+                                    ->checkbox([ 'class' => 'form-check-input',
+                                                'id' => 'exampleCheck10'])
+                                    ->label(false) ; ?>
                                     <label class="form-check-label" for="exampleCheck10"></label>
                                     <label for="exampleCheck10"
                                            class="profile-info__check-text job-resolution-checkbox">Вахтовый
@@ -333,14 +357,34 @@ $this->registerJsFile('js/jquery-editable-select.js');
                         </div>
                         <div class="col-lg-3 col-md-4 col-11">
                             <ul class="card-ul-radio profile-radio-list">
-                                <li>
+                                <!-- <li>
                                     <input type="radio" id="test9131" name="radio-group3123" checked="">
+
                                     <label for="test9131">Нет опыта работы</label>
                                 </li>
                                 <li>
                                     <input type="radio" id="test10242" name="radio-group3123">
+
                                     <label for="test10242">Есть опыт работы</label>
-                                </li>
+                                </li> -->
+                                <?= 
+                                    $form->field($model, 'exp')
+                                        ->radioList(
+                                            [0 => 'Нет опыта работы', 1 => 'Есть опыт работы'],
+                                            [
+                                                'item' => function($index, $label, $name, $checked, $value) {
+                                                    if ($checked) ;
+                                                    $html = '<li><input type="radio" id="exp'.$index.'" name="'.$name.'" value="'.$value.'"';
+                                                    if ($checked) $html .=' checked';
+                                                    $html .='>';
+                                                    $html .= '<label for="exp'.$index.'">'.$label.'</label></li>';
+
+                                                    return $html;
+                                                }
+                                            ]
+                                        )
+                                    ->label(false);
+                                ?>
                             </ul>
                         </div>
                     </div>
@@ -524,14 +568,16 @@ $this->registerJsFile('js/jquery-editable-select.js');
                             <div class="paragraph">О себе</div>
                         </div>
                         <div class="col-lg-5 col-md-7 col-12">
-                            <textarea class="dor-input w100 h176 mb8"></textarea>
+                            <!-- <textarea class="dor-input w100 h176 mb8"></textarea> -->
+                            <?= $form->field($model, 'about')->textarea(['class' => 'dor-input w100 h176 mb8'])->label(false) ; ?>
                         </div>
                     </div>
                     <div class="row mb128 mobile-mb64">
                         <div class="col-lg-2 col-md-3">
                         </div>
                         <div class="col-lg-10 col-md-9">
-                            <a href="#" class="orange-btn link-orange-btn">Сохранить</a>
+                            <!-- <a href="#" class="orange-btn link-orange-btn">Сохранить</a> -->
+                            <?= Html::submitButton('Сохранить', ['class' => 'orange-btn link-orange-btn']) ?>
                         </div>
                     </div>
                 <!-- </form> -->
